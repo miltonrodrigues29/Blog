f7f04f4c1a18cc9574d3acec7e19e80191ac6e77
Milton Rodrigues <rodriguesmilton21@gmail.com>
2022-04-07 23:13:00 +0530

    -

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,34 @@
+import "./post.css";
+
+export default function post() {
+  return (
+    <div className="post">
+      <img
+        className="postImg"
+        src="https://images.pexels.com/photos/2294361/pexels-photo-2294361.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+        alt=""
+      ></img>
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Music</span>
+          <span className="postCat">Life</span>
+        </div>
+        <span className="postTitle">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry.
+        </span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDesc">
+        Contrary to popular belief, Lorem Ipsum is not simply random text. It
+        has roots in a piece of classical Latin literature from 45 BC, making it
+        over 2000 years old. Richard McClintock, a Latin professor at
+        Hampden-Sydney College in Virginia, looked up one of the more obscure
+        Latin words, consectetur, from a Lorem Ipsum passage, and going through
+        the cites of the word in classical literature, discovered the
+        undoubtable source.
+      </p>
+    </div>
+  );
+}
